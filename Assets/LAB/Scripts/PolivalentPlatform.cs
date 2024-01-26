98c74fdc51f2aac95b1098c49ed48319750619df
Adrián <jagv94@gmail.com>
2024-01-26 15:14:30 +0100

    Flataforma Done!

--- Assets/LAB/Scripts/PolivalentPlatform.cs
+++ Assets/LAB/Scripts/PolivalentPlatform.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PolivalentPlatform : MonoBehaviour
 {
+    private enum PlatformState
+    {
+        TOFINAL,
+        TOORIGIN,
+        WAITINGONORIGIN,
+        WAITINGONFINAL
+    }
+
     [Header("Platform Type")]
     [Tooltip("Final position")] [SerializeField] private bool isLavaPlatform = false;
     [Tooltip("Final position")] [SerializeField] private bool lavaPlatformActivated = false;
@@ -14,150 +23,47 @@ public class PolivalentPlatform : MonoBehaviour
     [Header("Movement")]
     private Vector3 oPos;
     [Tooltip("Final position")] [SerializeField] private Vector3 fPos;
-    [Tooltip("Duration of displacement (origin to final)")] [SerializeField] private float dispDurationOToF = 2.0f;
-    [Tooltip("Duration of displacement (final to origin)")] [SerializeField] private float dispDurationFToO = 2.0f;
-    [Tooltip("Time before move (origin to final)")] [SerializeField] private float timeToStartOToF = 3.0f;
-    [Tooltip("Time before move (final to origin)")] [SerializeField] private float timeToStartFToO = 3.0f;
-    [Tooltip("Does the platform move from the origin to the end?")] [SerializeField] private bool movingOToF = true;
-    [Tooltip("Does the platform move from the end to the origin?")] [SerializeField] private bool movingFToO = false;
+    [Tooltip("Duration of displacement (origin to final)")] [SerializeField] private float dispDurationToFinal = 2.0f;
+    [Tooltip("Duration of displacement (final to origin)")] [SerializeField] private float dispDurationToOrigin = 2.0f;
+    [Tooltip("Time before move (origin to final)")] [SerializeField] private float waitTimeToMoveToFinal = 3.0f;
+    [Tooltip("Time before move (final to origin)")] [SerializeField] private float waitTimeToMoveToOrigin = 3.0f;
+    [Tooltip("Platform state")] [SerializeField] private PlatformState platformState;
     [Tooltip("Player has touched the platform?")] [SerializeField] private bool touchedByPlayer = false;
-    [Tooltip("Ogre has touched the platform?")] [SerializeField] private bool touchedByOgre = false;
-    private float timeToStartOToFBackup;
-    private float timeToStartFToOBackup;
     private float elapsedTime = 0.0f;
     private Vector3 prevPos; // Platform previous position
 
     [Header("Route prediction")]
     [Tooltip("Color of the predictive route line")][SerializeField] private Color predictionColor = Color.yellow;
 
+
     // Start is called before the first frame update
     void Start()
     {
         oPos = transform.position;
         elapsedTime = 0.0f;
-        timeToStartOToFBackup = timeToStartOToF;
-        timeToStartFToOBackup = timeToStartFToO;
+        platformState = PlatformState.WAITINGONORIGIN;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        elapsedTime += Time.deltaTime;
+        elapsedTime += Time.fixedDeltaTime;
 
-        #region PlatformMovement
-        if (isLavaPlatform)
-        {
-            if (lavaPlatformActivated)
-            {
-                timeToStartOToF -= Time.deltaTime;
-                if (timeToStartOToF <= 0.0f)
-                {
-                    movingOToF = true;
-                    if (movingOToF)
-                    {
-                        if (elapsedTime < dispDurationOToF)
-                        {
-                            // Calculate completed percentage
-                            float completed = elapsedTime / dispDurationOToF;
-
-                            // Lineal interpolation between start and final positions
-                            transform.position = Vector3.Lerp(oPos, fPos, completed);
-                        }
-                        else
-                        {
-                            // Movement finished
-                            transform.position = fPos;
-                            movingOToF = false;
-                            elapsedTime = 0.0f;
-                        }
-                    }
-                    else
-                    {
-                        timeToStartFToO -= Time.deltaTime;
-                        if (timeToStartFToO <= 0.0f)
-                        {
-                            movingFToO = true;
-                            if (movingFToO)
-                            {
-                                if (elapsedTime < dispDurationFToO)
-                                {
-                                    // Calculate completed percentage
-                                    float completed = elapsedTime / dispDurationFToO;
-
-                                    // Lineal interpolation between start and final positions
-                                    transform.position = Vector3.Lerp(fPos, oPos, completed);
-                                }
-                                else
-                                {
-                                    // Movement finished
-                                    transform.position = oPos;
-                                    movingFToO = false;
-                                    elapsedTime = 0.0f;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if(!movingOToF && !movingFToO)
-                {
-                    timeToStartFToO = timeToStartFToOBackup;
-                    timeToStartOToF = timeToStartOToFBackup;
-                    lavaPlatformActivated = false;
-                }
-            }
-        }
-        else
-        {
-            if (movingOToF)
-            {
-                if (elapsedTime < dispDurationOToF)
-                {
-                    // Calculate completed percentage
-                    float completed = elapsedTime / dispDurationOToF;
-
-                    // Lineal interpolation between start and final positions
-                    transform.position = Vector3.Lerp(oPos, fPos, completed);
-                }
-                else
-                {
-                    // Movement finished
-                    transform.position = fPos;
-                    movingOToF = false;
-                    elapsedTime = 0.0f;
-                }
-            }
-            else
-            {
-                if (elapsedTime < dispDurationFToO)
-                {
-                    // Calculate completed percentage
-                    float completed = elapsedTime / dispDurationFToO;
-
-                    // Lineal interpolation between start and final positions
-                    transform.position = Vector3.Lerp(fPos, oPos, completed);
-                }
-                else
-                {
-                    // Movement finished
-                    transform.position = oPos;
-                    movingOToF = true;
-                    elapsedTime = 0.0f;
-                }
-            }
-        }
-        #endregion
+        PlatformMovement();
 
         #region Collision actions
         if (touchedByPlayer)
         {
             if (player != null)
             {
-                // Calcular el desplazamiento de la plataforma y la diferencia entre la posición actual y la anterior
-                Vector3 displacement = transform.position - prevPos;
+                if (platformState == PlatformState.TOFINAL || platformState == PlatformState.TOORIGIN)
+                {
+                    // Calcular el desplazamiento de la plataforma y la diferencia entre la posición actual y la anterior
+                    Vector3 displacement = transform.position - prevPos;
 
-                // Mover al jugador con el mismo desplazamiento que la plataforma
-                player.Move(displacement);
+                    // Mover al jugador con el mismo desplazamiento que la plataforma
+                    player.Move(displacement);
+                }
             }
         }
 
@@ -173,14 +79,8 @@ public class PolivalentPlatform : MonoBehaviour
             touchedByPlayer = true;
             if (isLavaPlatform && !lavaPlatformActivated)
             {
-                timeToStartOToF = timeToStartOToFBackup;
                 lavaPlatformActivated = true;
             }
-        }
-
-        if (collider.gameObject.CompareTag("Ogre"))
-        {
-            touchedByOgre = true;
         }
     }
 
@@ -190,11 +90,6 @@ public class PolivalentPlatform : MonoBehaviour
         {
             touchedByPlayer = false;
         }
-
-        if (collider.gameObject.CompareTag("Ogre"))
-        {
-            touchedByOgre = false;
-        }
     }
 
     // Draw line from oPosition to fPosition
@@ -202,5 +97,91 @@ public class PolivalentPlatform : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawLine(transform.position, fPos);
+    }
+
+    void PlatformMovement()
+    {
+        switch (platformState)
+        {
+            case PlatformState.TOFINAL:
+                SlidingMovement(oPos, fPos, dispDurationToFinal);
+                break;
+
+            case PlatformState.TOORIGIN:
+                SlidingMovement(fPos, oPos, dispDurationToOrigin);
+                break;
+
+            case PlatformState.WAITINGONORIGIN:
+                if (!isLavaPlatform) { platformState = PlatformState.TOFINAL; }
+                else
+                {
+                    PlatformWaitingToBeTouched(ref waitTimeToMoveToFinal);
+                }
+                break;
+
+            case PlatformState.WAITINGONFINAL:
+                if (!isLavaPlatform) { platformState = PlatformState.TOORIGIN; }
+                else
+                {
+                    PlatformWaitingToBeTouched(ref waitTimeToMoveToOrigin);
+                }
+                break;
+        }
+    }
+
+    void SlidingMovement(Vector3 origin, Vector3 destiny, float timeToDestiny)
+    {
+        if (elapsedTime < timeToDestiny)
+        {
+            // Calculate completed percentage
+            float completed = elapsedTime / timeToDestiny;
+
+            // Lineal interpolation between start and final positions
+            transform.position = Vector3.Lerp(origin, destiny, completed);
+        }
+        else
+        {
+            // Movement finished
+            transform.position = destiny;
+            elapsedTime = 0.0f;
+
+            if (platformState == PlatformState.TOFINAL)
+            {
+                if (!isLavaPlatform) platformState = PlatformState.TOORIGIN;
+                else platformState = PlatformState.WAITINGONFINAL;
+            }
+            else if (platformState == PlatformState.TOORIGIN)
+            {
+                if (!isLavaPlatform) platformState = PlatformState.TOFINAL;
+                else
+                {
+                    platformState = PlatformState.WAITINGONORIGIN;
+                    lavaPlatformActivated = false;
+                }
+            }
+        }
+    }
+
+    void PlatformWaitingToBeTouched (ref float timeToWait)
+    {
+        float timeBackup = timeToWait;
+        if (lavaPlatformActivated)
+        {
+            timeToWait -= Time.fixedDeltaTime;
+            if (timeToWait <= 0.0f)
+            {
+                elapsedTime = 0.0f;
+                if (platformState == PlatformState.WAITINGONORIGIN)
+                {
+                    timeToWait = timeBackup;
+                    platformState = PlatformState.TOFINAL;
+                }
+                else if (platformState == PlatformState.WAITINGONFINAL)
+                {
+                    timeToWait = timeBackup;
+                    platformState = PlatformState.TOORIGIN;
+                }
+            }
+        }
     }
 }
