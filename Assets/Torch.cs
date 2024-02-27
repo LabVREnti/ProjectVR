b9cb65869bb911569227661a589df23a7b1a09f2
Nerea <nerea.martinez@enti.cat>
2024-02-27 18:07:38 +0100

    cofre con llave girando y respawn de antorcha

--- Assets/Torch.cs
+++ Assets/Torch.cs
@@ -1,3 +1,4 @@
+using Autohand;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,22 @@ using UnityEngine;
 public class Torch : MonoBehaviour
 {
     [SerializeField] GameObject fire;
+    [SerializeField] Transform holder;
+    Grabbable grabbable;
     float timer = 0;
+   [SerializeField] PlacePoint placePoint;
 
+    private void Start()
+    {
+        grabbable = GetComponent<Grabbable>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            Invoke("RespawnT", 3.0f);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,8 +30,9 @@ public class Torch : MonoBehaviour
             setFire(true);
         }
     }
-    private void Update()
+    void RespawnT()
     {
+        placePoint.Place(grabbable);
         
     }
 
